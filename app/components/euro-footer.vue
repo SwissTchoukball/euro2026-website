50fb71b9a3230b9af6724da55a66cab07f9262d8
David Sandoz <david.sandoz@tchoukball.ch>
2026-01-24 15:52:27 +0100

    feat(footer): add sponsors

--- app/components/euro-footer.vue
+++ app/components/euro-footer.vue
@@ -1,5 +1,45 @@
 <template>
-  <footer>
+  <footer class="c-footer">
+    <div class="c-footer__sponsors">
+      <h3 class="t-headline-3">{{ $t("sponsors.main") }}</h3>
+      <ul class="u-unstyled-list c-footer__sponsors-list">
+        <li>
+          <a
+            href="https://www.linkedin.com/company/pro-gaïa-invest-sa"
+            class="c-footer__sponsor c-footer__sponsor--main"
+          >
+            <img
+              class="c-footer__sponsor-logo"
+              src="/images/logo-pro-gaia-invest.png"
+              alt="Logo of Pro Gaïa Invest SA"
+            />
+          </a>
+        </li>
+      </ul>
+
+      <h3 class="t-headline-4">{{ $t("sponsors.operational") }}</h3>
+      <ul class="u-unstyled-list c-footer__sponsors-list">
+        <li>
+          <a
+            href="https://baspo.admin.ch"
+            class="c-footer__sponsor c-footer__sponsor--secondary c-footer__sponsor--ofspo"
+          >
+            <img
+              class="c-footer__sponsor-logo"
+              src="/images/logo-swiss-confederation.svg"
+              alt="Logo of the Swiss Confederation"
+            />
+            <div class="c-footer__sponsor-ofspo-text">{{ $t("sponsors.federalOfficeOfSport") }}</div>
+          </a>
+        </li>
+        <li>
+          <a href="https://peerdom.com" class="c-footer__sponsor c-footer__sponsor--secondary">
+            <img class="c-footer__sponsor-logo" src="/images/logo-peerdom.svg" alt="Logo of Peerdom" />
+          </a>
+        </li>
+      </ul>
+    </div>
+
     <div class="c-footer__social-icons">
       <a
         href="https://instagram.com/euro2026tchoukball"
@@ -48,7 +88,68 @@
 </template>
 
 <style scoped>
+.c-footer {
+  margin-block: var(--euro-spacing-8);
+}
+
+.c-footer__sponsors {
+  text-align: center;
+  padding-block: var(--euro-spacing-8);
+  background-color: var(--euro-gray-050);
+
+  h3:not(:first-child) {
+    margin-top: var(--euro-spacing-6);
+  }
+}
+
+.c-footer__sponsors-list {
+  margin-top: var(--euro-spacing-4);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: var(--euro-spacing-8);
+}
+
+.c-footer__sponsor {
+  display: block;
+  text-decoration: none;
+  color: black;
+
+  &.c-footer__sponsor--main {
+    height: 6rem;
+    width: 12rem;
+  }
+
+  &.c-footer__sponsor--secondary {
+    height: 4rem;
+    width: 8rem;
+  }
+
+  /* Although they're secondary, just to make their logo readable, we make it bigger. */
+  &.c-footer__sponsor--ofspo {
+    height: 6rem;
+    width: 12rem;
+  }
+}
+
+.c-footer__sponsor-logo {
+  height: 100%;
+  width: 100%;
+  object-fit: contain;
+}
+
+.c-footer__sponsor-ofspo-text {
+  text-align: left;
+  font-weight: bold;
+  font-size: 0.6rem;
+  padding-left: 37px;
+  transform: translateY(-20px);
+}
+
 .c-footer__social-icons {
+  padding-top: var(--euro-spacing-4);
+
   display: flex;
   width: 100%;
   justify-content: center;
