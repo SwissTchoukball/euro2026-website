bf7a4c198b9e9911b6c1d711e441d290a50d77bd
David Sandoz <david.sandoz@tchoukball.ch>
2026-04-17 18:52:55 +0200

    fix(home): update imports for countdown

--- app/components/euro-countdown.vue
+++ app/components/euro-countdown.vue
@@ -21,7 +21,7 @@
 </template>
 
 <script setup lang="ts">
-import type { DateTimeArray } from "@maas/vue-equipment/composables";
+import { useCountdown, type DateTimeArray } from "@maas/vue-equipment/composables/useCountdown";
 import JSConfetti from "js-confetti";
 
 let jsConfetti: JSConfetti;
